d4a8036dac5fc5d6296e2b7b065b6d40e4cf7815
KIM SANGYEUB <withlight@yugo-no-MacBook-Air.local>
2017-12-02 22:40:52 +0900

    modified for utf8

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template, request, Flask, flash, redirect, url_for, session
-
+import sys
 import db_connect as db
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
 	if request.method == "POST":
-		new_word = request.form["id"]
-		db.add_words(new_word)
+		new_word = request.form["word"].encode('utf-8')
 		words = [new_word]
-		words += db.get_words(4)
+		for word in db.get_words(4):
+			words.append(word[0])
+		db.add_words(new_word)
 	else:
-		words = db.get_words(5)
+		words = []
+		for word in db.get_words(5):
+			words.append(word[0])
 	return render_template("index.html", words=words)
 
 if __name__ == "__main__":
